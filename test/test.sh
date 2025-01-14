2a1b523b2130a165dbdc4c3482113f45bcceaf56
Lars Brinkhoff <lars@nocrew.org>
2025-01-14 13:21:34 +0100

    Update test.

--- test/test.sh
+++ test/test.sh
@@ -26,7 +26,7 @@ test -x simh/BIN/h316 || (build)
 echo Starting ARPANET.   Allow 30 seconds for IMP network to settle.
 ./arpanet.sh start
 # Some time for NCP too.
-sleep 10
+sleep 3
 
 echo "Test pinging another host."
 NCP=ncp2 $APPS/ncp-ping -c3 003 | grep 'Reply from host 003: seq=3' || fail
@@ -37,15 +37,15 @@ NCP=ncp2 $APPS/ncp-ping -c1 004 && fail
 echo "Test pinging a dead IMP."
 NCP=ncp2 $APPS/ncp-ping -c1 005 && fail
 
+echo "Test RFC to socket without server."
+NCP=ncp3 $APPS/ncp-finger 002 && fail
+
 echo "Test ICP and simple data transfer using the Finger protocol."
-NCP=ncp2 $APPS/ncp-finser &
+NCP=ncp2 $APPS/ncp-finser || fail &
 PID=$!
 sleep 1
-NCP=ncp3 $APPS/ncp-finger 002 "Sample Finger command from client." &
-sleep 10
+NCP=ncp3 $APPS/ncp-finger 002 "Sample Finger command from client." || fail &
+sleep 3
 kill $! $PID 2>/dev/null || :
-
-echo "Test RFC to socket without server."
-NCP=ncp3 $(APPS)/ncp-finger 002
 
 exit $RESULT
