0001b791b5d53d8d6e05fa2bd25b3fbe5b3bbf18
Lars Brinkhoff <lars@nocrew.org>
2025-01-14 13:21:34 +0100

    Update test.

--- test/test.sh
+++ test/test.sh
@@ -37,15 +37,15 @@ NCP=ncp2 $APPS/ncp-ping -c1 004 && fail
 echo "Test pinging a dead IMP."
 NCP=ncp2 $APPS/ncp-ping -c1 005 && fail
 
+echo "Test RFC to socket without server."
+NCP=ncp3 $APPS/ncp-finger 002 && fail
+
 echo "Test ICP and simple data transfer using the Finger protocol."
-NCP=ncp2 $APPS/ncp-finser &
+NCP=ncp2 $APPS/ncp-finser || fail &
 PID=$!
 sleep 1
-NCP=ncp3 $APPS/ncp-finger 002 "Sample Finger command from client." &
-sleep 10
+NCP=ncp3 $APPS/ncp-finger 002 "Sample Finger command from client." || fail &
+sleep 3
 kill $! $PID 2>/dev/null || :
-
-echo "Test RFC to socket without server."
-NCP=ncp3 $(APPS)/ncp-finger 002
 
 exit $RESULT
