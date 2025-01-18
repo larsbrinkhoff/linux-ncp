6b31aa22185e1e461e87b5f2f22d475598bb4b31
Lars Brinkhoff <lars@nocrew.org>
2025-01-18 19:59:39 +0100

    Test a TELNET connection.

--- test/test.sh
+++ test/test.sh
@@ -23,7 +23,7 @@ build() {
 test -r simh/scp.c || git submodule update --init
 test -x simh/BIN/h316 || (build)
 
-echo Starting ARPANET.   Allow 30 seconds for IMP network to settle.
+echo "Starting ARPANET.  Allow 30 seconds for IMP network to settle."
 ./arpanet.sh start
 # Some time for NCP too.
 sleep 10
@@ -39,6 +39,7 @@ NCP=ncp2 $APPS/ncp-ping -c1 005 && fail
 
 echo "Test RFC to socket without server."
 NCP=ncp3 $APPS/ncp-finger 002 && fail
+sleep 2
 
 echo "Test ICP and simple data transfer using the Finger protocol."
 NCP=ncp2 $APPS/ncp-finser || fail &
@@ -47,5 +48,14 @@ sleep 1
 NCP=ncp3 $APPS/ncp-finger 002 "Sample Finger command from client." || fail &
 sleep 3
 kill $! $PID 2>/dev/null || :
+
+echo "Test a TELNET session."
+
+NCP=ncp2 $APPS/ncp-telnet -bs || fail &
+PID=$!
+sleep 1
+(echo test; sleep 1) | NCP=ncp3 $APPS/ncp-telnet -bc 002 | grep -a Welcome || fail &
+sleep 3
+kill $! $PID 2>/dev/null && fail
 
 exit $RESULT
